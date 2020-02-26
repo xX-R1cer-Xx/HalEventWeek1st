66b7586bef5fa28f35c0f0f5066da99fcf4ae987
xX-R1cer-Xx <45945870+xX-R1cer-Xx@users.noreply.github.com>
2020-02-27 03:50:58 +0900

    Add files via upload

--- HEW/js/index.js
+++ HEW/js/index.js
@@ -1,1 +1,10 @@
-$('.slider').slick();
+$('.slider').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    draggable: true,
+    speed: 400,
+    fade: true,
+    arrows: false,
+    cssEase: 'linear'
+});
